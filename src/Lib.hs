9800b5ab3abe8f000bd0113d0bb3f5464eb18e53
RyanBeatty <rvbeatty@email.wm.edu>
2017-08-27 17:17:44 -0700

    Removed Color data type. Started fleshing out the processId interface

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,13 +3,15 @@ module Lib
     ( someFunc
     ) where
 
-class ProcessId a
+-- Interface for the 'Color' abstraction that Spezialleti and Kearns
+-- describes in their paper. Here the 'White' process id acts as a sentinal
+-- process id.
+class ProcessId a where
+        -- Is the process id the 'White' process id.
+        isWhiteId :: a -> Bool
 
 -- Wrapper around Integer type.
 newtype Count = Count { getCount :: Integer }
-  deriving (Show, Eq)
-
-data Color = White | Red
   deriving (Show, Eq)
 
 class Channel a
@@ -18,7 +20,7 @@ class Channel a
 data ProcessState a b where
         ProcessState  :: (ProcessId a, Channel b) =>
                          { selfId        :: a     -- The proccess id of the process currently running.
-                         , selfColor     :: Color -- The current color of the process.
+                         , localColor    :: a     -- The current color of the process.
                          , opCount       :: Count -- The number of operations that have been executed on this process.
                          , snapshotCount :: Count -- The number of snapshots that this process has been involved in.
                          , inChannels    :: [b]   -- All incoming channels to this process.
