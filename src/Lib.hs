818713fe88b7279f3ec6c1e89cbe4547f32432ed
RyanBeatty <rvbeatty@email.wm.edu>
2017-08-29 16:48:26 -0700

    changed type parameter names to be more intuitive

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,11 +21,11 @@ class Channel a where
         receiveFrom :: a -> ()
 
 -- Type Parameters:
--- ProcessId a
-data Message a =
+-- ProcessId p
+data Message p =
   -- A warning message is sent to alert processes that a snapshot has begun.
-  WarningMsg { warningColor :: a -- The color of the initiator of the current snapshot.
-             , senderColor  :: a -- The color of the process that sent this warning message.
+  WarningMsg { warningColor :: p -- The color of the initiator of the current snapshot.
+             , senderColor  :: p -- The color of the process that sent this warning message.
              }
   deriving (Show, Eq)
 
@@ -35,30 +35,33 @@ data ProcessConfig = ProcessConfig
 -- A Letter contains a Message to send and the necessary info to be able to
 -- actually send the message to a process.
 -- Type Parameters:
--- (Channel a, ProcessId b)
-data Letter a b = Letter { senderOf    :: a -- The channel to send the response accross.
-                         , recipientOf :: a -- The channel to send the message across.
-                         , msg         :: Message b  -- The message payload.
+-- (Channel c, ProcessId p)
+data Letter c p = Letter { senderOf    :: c -- The channel to send the response accross.
+                         , recipientOf :: c -- The channel to send the message across.
+                         , msg         :: Message p  -- The message payload.
                          } 
   deriving (Show, Eq)
 
 -- The state of a Process.
 -- Type Parameters:
--- (ProcessId a, Channel b)
-data ProcessState a b = ProcessState { idColor       :: a     -- The color that uniquely identifies the process.
-                                     , localColor    :: a     -- The current color of the process.
+-- (ProcessId p, Channel c)
+data ProcessState p c = ProcessState { idColor       :: p     -- The color that uniquely identifies the process.
+                                     , localColor    :: p     -- The current color of the process.
                                      , opCount       :: Count -- The number of operations that have been executed on this process.
                                      , snapshotCount :: Count -- The number of snapshots that this process has been involved in.
-                                     , inChannels    :: [b]   -- All incoming channels to this process.
-                                     , outchannels   :: [b]   -- All outgoing channels from this process.
-                                     , warningRecSet :: S.Set b -- Set of channels that have sent a warning to this process.
-                                     , idBorderSet   :: S.Set a -- Set of process ids that belong to neighboring master initiator processes.
+                                     , inChannels    :: [c]   -- All incoming channels to this process.
+                                     , outchannels   :: [c]   -- All outgoing channels from this process.
+                                     , warningRecSet :: S.Set c -- Set of channels that have sent a warning to this process.
+                                     , idBorderSet   :: S.Set p -- Set of process ids that belong to neighboring master initiator processes.
                                      }
   deriving (Show, Eq)
 
 -- Type Parameters:
--- (ProcessId a, Channel b, c)
-newtype ProcessAction a b c = ProcessAction { runAction :: RWS ProcessConfig [Letter b a] (ProcessState a b) c }
+-- (ProcessId a, Channel b, x)
+newtype ProcessAction p c x = ProcessAction { runAction :: RWS ProcessConfig [Letter c p] (ProcessState p c) x }
+
+msgHandler :: (Channel c, ProcessId p) => Letter c p -> ProcessAction p c ()
+msgHandler = undefined
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
